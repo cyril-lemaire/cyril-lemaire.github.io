bf75e034a5c53eecc77c10f3e7655d28199c14ef
cyril-lemaire <cyril.lemaire@wanadoo.fr>
2018-11-29 19:04:27 +0100

    Starting to add projects

---
+++ Project_Skeleton.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php PROJECT_NAME ?></title>
+</head>
+
+<body>
+	<header>
+		<h1><?php PROJECT_NAME ?></h1>
+	</header>
+	<footer>
+		<a href="projects.html">Back to projects list</a>
+	</footer>
+</body>
+</html>
